1c61dee83690670ad66e41afbb28273b48d5811b
lucas <lucas@example.com>
2024-03-21 18:28:49 -0400

    js loading works

--- web/main.js
+++ web/main.js
@@ -1,37 +1,30 @@
 import { app } from "../../scripts/app.js"
-import { api } from "../../scripts/api.js"
 import { ComfyWidgets } from "../../scripts/widgets.js"
-//import { ace } from "./ace/ace.js"
 
 const extension_name = "dandy"
-const extension_webroot = "/extensions/dandy/"
-const extension_css = `${extension_webroot}dandy.css`
+const dandy_webroot = "/extensions/dandy/"
+const dandy_css = `${dandy_webroot}dandy.css`
+
 const JS_TYPE = 'JS_URLS'
 const JS_NAME = 'js'
 
-const loadDandyCss = (doc) => {
-  const dandycss = doc.createElement("link")
-  dandycss.rel = 'stylesheet'
-  dandycss.type = 'text/css'
-  dandycss.href = `${extension_css}`
-  doc.head.appendChild(dandycss)
+const load_css = (doc, url) => {
+  const link = doc.createElement("link")
+  link.rel = 'stylesheet'
+  link.type = 'text/css'
+  link.href = url
+  doc.head.appendChild(link)
 }
 
-const connectJsIo = (dandy) => {
-  const { node } = dandy
-  node.addInput(JS_NAME, JS_TYPE)
-  node.addOutput(JS_NAME, JS_TYPE)
-  dandy.in_slot_javascript = node.findInputSlot(JS_NAME)
-  dandy.out_slot_javascript = node.findOutputSlot(JS_NAME)
+const load_dandy_css = (doc) => {
+  load_css(doc, dandy_css)
 }
 
-const dandyCssLink = `<link rel="stylesheet" type="text/css" href="${extension_css}" />`
+const dandyCssLink = `<link rel="stylesheet" type="text/css" href="${dandy_css}" />`
 
 let DANDY_INITIALIZED = false
 const initDandy = () => {
-  console.log("initDandy()")
-  
-  loadDandyCss(document)
+  load_dandy_css(document)
 
   const pairwise = (a) => {
     const x = []
@@ -56,14 +49,14 @@ const initDandy = () => {
 
   pairwise(features_and_codes).forEach(async pair => {
     const [feature, js_] = pair
-    const response = await fetch(`${extension_webroot}ace/${js_}`)
+    const response = await fetch(`${dandy_webroot}ace/${js_}`)
     const text = await response.text()
     const blob = new Blob([text], { type: 'application/javascript' })
     const url = URL.createObjectURL(blob)
     ace.config.setModuleUrl(`ace/${feature}`, url)
-    console.log(`${i_feature}/${n_features}:ace.config.setModuleUrl("ace/${feature}", ${url})`)
+    //console.log(`${i_feature}/${n_features}:ace.config.setModuleUrl("ace/${feature}", ${url})`)
     if (++i_feature == n_features)
-      console.log("DANDY_INITIALIZED")
+      //console.log("DANDY_INITIALIZED")
       DANDY_INITIALIZED = true
   })
 }
@@ -77,18 +70,17 @@ const simple_widget_getter_setter = (el) => {
 }
 
 const add_simple_node = (node, type, el) => {
-  console.log("node.addDOMWidget(", el.id, type, el, simple_widget_getter_setter(el), ")")
   return node.addDOMWidget(el.id, type, el, simple_widget_getter_setter(el))
 }
 
 class DandyEditor {
   static i_editor = 0
 
-  constructor(node) {
+  constructor(node, app) {
     this.node = node
-    connectJsIo(this)
+    this.app = app
+    this.js_chain = new DandyJsChain(this, node, app)
 
-    console.log("initDandyEditorNode(node)", node)
     const node_type = 'dandy_editor'
     const editor_id = `${node_type}_${DandyEditor.i_editor++}`
 
@@ -119,17 +111,16 @@ class DandyEditor {
     editor.setOption('hasCssTransforms', true)
 
     editor_pre.addEventListener("resize", (event) => {
-      console.log('Event type:', event.type)
-      console.log('Event details:', event)
       editor.resize()
     })
   }
 }
 
 class DandyCanvas {
-  constructor(node) {
+  constructor(node, app) {
     this.node = node
-    connectJsIo(this)
+    this.app = app
+    this.js_chain = new DandyJsChain(this, node, app)
 
     this.dandyland = null
     this.widget = null
@@ -149,7 +140,7 @@ class DandyCanvas {
 
     for (let i_file = 0; i_file < n_files; i_file++) {
       const js_ = js_files[i_file]
-      const response = await fetch(`${extension_webroot}${js_}`)
+      const response = await fetch(`${dandy_webroot}${js_}`)
       const text = await response.text()
       const blob = new Blob([text], { type: 'application/javascript' })
       const url = URL.createObjectURL(blob)
@@ -170,41 +161,46 @@ class DandyCanvas {
   }
 }
 
+// ==========================================================================================
 class DandyJsLoader {
-  constructor(node) {
+  constructor(node, app) {
     this.node = node
-    connectJsIo(this)
+    this.app = app
+    this.js_chain = new DandyJsChain(this, node, app)
 
-    this.files = []
+    this.files_order = []
+    this.filemap = {}
     this.urlmap = {}
-    this.js_blobs = []
+    this.blobmap = {}
+
     this.serialize_widgets = true
     this.isVirtualNode = true
       
     const file_input = document.createElement("input")
     const js_files_selected = async () => {
       if (file_input.files.length) {
-        console.log("this.add_js_files(file_input.files)", file_input, file_input.files)
         await this.add_js_files(file_input.files)
       }
     }
 
     Object.assign(file_input, {
       type: "file",
+      multiple: true,
       accept: "application/javascript",
       style: "display: none",
       onchange: js_files_selected,
     })
     document.body.appendChild(file_input)
 
+    this.file_input = file_input
+
     const load_js_widget = node.addWidget("button", "JAVASCRIPT", "javascript", () => {
       file_input.click()
     })
     load_js_widget.label = "Choose files..."
     load_js_widget.serialize = false
-    console.log('added widget', load_js_widget)
 
-    const files_div = document.createElement("div")
+    const files_div = document.createElement("pre")
     files_div.classList.add("dandyMax")
     
     const files_widget = add_simple_node(node, "div", files_div)
@@ -213,8 +209,70 @@ class DandyJsLoader {
     const ul = document.createElement('ul')
     ul.classList.add('dandyUL')
     files_div.appendChild(ul)
+    this.ul_filelist = ul
+  }
 
-    this.files.forEach((file, i) => {
+  reset_file_input() {
+    this.file_input.value = null; // Reset the value to clear selected files
+  }
+
+  move_up(filename) {
+    const { files_order } = this
+    const i = files_order.indexOf(filename)
+    const j = i - 1
+    const exists_and_not_at_front = i > 0
+    
+    if (exists_and_not_at_front) {
+      const x = files_order[j]
+      files_order[j] = files_order[i]
+      files_order[i] = x
+    }
+
+    this.render_file_list()
+    this.update_chain()
+  }
+
+  move_down(filename) {
+    const { files_order } = this
+    const i = files_order.indexOf(filename)
+    const j = i + 1
+    const found = i >= 0
+    const not_at_end = i < files_order.length - 1
+    
+    if (found && not_at_end) {
+      const x = files_order[j]
+      files_order[j] = files_order[i]
+      files_order[i] = x
+    }
+
+    this.render_file_list()
+    this.update_chain()
+  }
+  
+  remove_file(filename) {
+    const { blobmap, urlmap, filemap } = this
+  
+    this.files_order = this.files_order.filter( (x) => x !== filename )
+    URL.revokeObjectURL(urlmap[filename])
+    urlmap[filename] = undefined
+    filemap[filename] = undefined
+    blobmap[filename] = undefined
+
+    this.reset_file_input()
+    this.render_file_list()
+    this.update_chain()
+  }
+
+  render_file_list() {
+    const { files_order, ul_filelist } = this
+
+    ul_filelist.innerHTML = ''
+    const n_files = files_order.length
+    if (n_files === 0) {
+      return
+    }
+
+    files_order.forEach((filename, i) => {
       const li = document.createElement('li')
       const file_span = document.createElement('span')
       const move_up_button = document.createElement('span')
@@ -227,134 +285,242 @@ class DandyJsLoader {
       move_down_button.classList.add('dandySpanButton')
       remove_button.classList.add('dandySpanButton')
 
-      file_span.innerText = file
-      move_up_button.innerHTML = "(up)"
-      move_down_button.innerHTML = "(down)"
-      remove_button.innerHTML = "(remove)"
+      file_span.innerText = filename
+
+      move_up_button.innerHTML = '↑'
+      move_down_button.innerHTML = '↓'
+      remove_button.innerHTML = '✖'
+
+      const first = i === 0
+      const last = i === n_files - 1
+      move_up_button.style.display = first ? "none" : "inline"
+      move_down_button.style.display = last ? "none" : "inline"
+
+      move_up_button.onclick   = () => {this.move_up(filename)}
+      move_down_button.onclick = () => {this.move_down(filename)}
+      remove_button.onclick    = () => {this.remove_file(filename)}
 
       li.append(file_span, move_up_button, move_down_button, remove_button)
-      ul.append(li)
+      ul_filelist.append(li)
     })
     
+  }
+
+  update_chain() {
+    const { files_order, urlmap, js_chain } = this
+    let s = ''
+    files_order.forEach((filename) => {
+      const url = urlmap[filename]
+      s += `${url}\n`
+    })
+    js_chain.js_urls = s
+    js_chain.update_chain()
   }
 
   async add_js_files(chosen_files) {
-    const { files, urlmap, js_blobs } = this
-    for (let i = 0; i < chosen_files.length; ++i) {
-      console.log("files", files)
-      files.push(chosen_files[i])
+    const { files_order, filemap, urlmap, blobmap } = this
+
+    let i_file = 0
+    const n_files = chosen_files.length
+    
+    // foreach doesn't work :()
+    for (let i = 0; i < n_files; ++i) {
+      const file = chosen_files[i]
+      const { name, size, lastModified } = file
+
+      if (files_order.indexOf(name) === -1) {
+        files_order.push(name)
+      }
+      
+      const reader = new FileReader()
+      reader.onload = (event) => {
+        const scriptText = event.target.result
+        const blob = new Blob([scriptText], { type: 'application/javascript' })
+        const url = URL.createObjectURL(blob)
+
+        const existing = urlmap[name]
+        if (existing) {
+          URL.revokeObjectURL(existing)
+        }
+
+        filemap[name] = file
+        blobmap[name] = blob
+        urlmap[name] = url
+
+        if (++i_file === n_files) {
+          this.update_chain()
+          this.render_file_list()
+        }
+      }
+  
+      reader.readAsText(file)
     }
 
-    for (let i = 0; i < files.length; ++i) {
-      const file = files[i]
-      console.log("FILE", file)
-      if (urlmap[file] === undefined) {
-        const response = await fetch(`${file}`)
-        const text = await response.text()
-        const blob = new Blob([text], { type: 'application/javascript' })
-        const url = URL.createObjectURL(blob)
-        js_blobs.push(blob)
-        urlmap[file] = url
-      }
-    }
   }
 }
 
-const find_last_js_in_chain = (dandy, f_taking_dandy, seen = []) => {
-  const { node, out_slot_javascript } = dandy
-  const { graph, outputs } = node
-  const output = outputs[out_slot_javascript]
-  const { links } = output
-  
-  if (links === null || links.length === 0) {
-    f_taking_dandy(dandy)
-    return
+// ==========================================================================================
+class DandyJsChain {
+  static debug_blobs = false
+
+  constructor(dandy, node, app) {
+    this.dandy = dandy
+    this.node = node
+    this.app = app
+    this.js_urls = ""
+
+    node.addInput(JS_NAME, JS_TYPE)
+    node.addOutput(JS_NAME, JS_TYPE)
+    this.in_slot_javascript = node.findInputSlot(JS_NAME)
+    this.out_slot_javascript = node.findOutputSlot(JS_NAME)
+
+    if (DandyJsChain.debug_blobs) {
+      this.urls_widget = ComfyWidgets.STRING(node, "urls", ["", {default:"", multiline: true}], app)
+      this.urls_widget.serialize = false
+    }
+
+    node.onConnectionsChange = (type, index, connected, link_info) => {
+      this.follow_js_chain((js_chain) => {
+        js_chain.update_js()
+      })
+    }
   }
 
-  links.forEach( (link_id) => {
-    const link = graph.links[link_id]
-    const { target_id } = link
+  // f_each_node: (js_chain) => {}
+  follow_js_chain(f_each_node, seen = []) {
+    const { node, out_slot_javascript } = this
+    const { graph, outputs } = node
+    const output = outputs[out_slot_javascript]
+    const { links } = output
     
-    let loopy = false
-    seen.forEach( (seen_target_id) => {
-      if (seen_target_id === target_id) {
-        loopy = true
-      }
-    })
-    if (loopy) {
-      console.error("loop in graph, bailing")
+    f_each_node(this)
+
+    if (links === null || links.length === 0) {
+      // we've reached the end
       return
     }
-    const seen_prime = seen.concat([target_id])
 
-    // console.log("TARGET_ID", link.target_id)
-    const target_node = graph.getNodeById(link.target_id)
-    const target_dandy = target_node.dandy
-    find_last_js_in_chain(target_dandy, f_taking_dandy, seen_prime)
-  })
+    links.forEach( (link_id) => {
+      const link = graph.links[link_id]
+      if (link === undefined) {
+        // node was likely removed and that's why we're firing
+        return
+      }
+
+      const { target_id } = link
+      
+      let loopy = false
+      seen.forEach( (seen_target_id) => {
+        if (seen_target_id === target_id) {
+          loopy = true
+        }
+      })
+      if (loopy) {
+        console.error("loop in graph, bailing")
+        return
+      }
+      const seen_prime = seen.concat([target_id])
+
+      const target_node = graph.getNodeById(link.target_id)
+      const target_dandy = target_node.dandy
+      const target_chain = target_dandy.js_chain
+      target_chain.follow_js_chain(f_each_node, seen_prime)
+    })
+  }
+
+  update_chain() {
+    this.follow_js_chain((js_chain) => {
+      js_chain.update_js()
+    })
+  }
+
+  update_js() {
+    const { in_slot_javascript, out_slot_javascript, node, js_urls } = this
+
+    let out_urls = ""
+    if (node.isInputConnected(in_slot_javascript)) {
+      const force_update = false
+      const in_urls = node.getInputData(in_slot_javascript, force_update)
+      out_urls += `${in_urls}\n`
+    }
+
+    out_urls += js_urls
+    node.setOutputData(out_slot_javascript, out_urls)
+    node.triggerSlot(out_slot_javascript)
+
+    if (DandyJsChain.debug_blobs) {
+      this.urls_widget.widget.element.value = out_urls
+    }
+  }
 }
 
+// ==========================================================================================
 class DandyP5JsLoader {
-  
-  constructor(node) {
+  constructor(node, app) {
     this.node = node
-    connectJsIo(this)
+    this.app = app
+    this.js_chain = new DandyJsChain(this, node, app)
 
-    node.size = [180, 55]
+    if (DandyJsChain.debug_blobs) {
+      node.size = [380, 100]
+    } else {
+      node.size = [290, 75]
+    }
 
     this.js_blob = null
     this.js_url = ""
     this.serialize_widgets = true
     this.isVirtualNode = true
-    this.loaded = false
 
-    this.update_js = this.update_js.bind(this)
+    const texty = document.createElement("pre")
+    texty.classList.add("dandyMax")
+    this.texty = texty
 
-    console.log("onConnectionsChange", node.onConnectionsChange)
-    node.onConnectionsChange = (type, index, connected, link_info) => {
-      find_last_js_in_chain(this, (dandy) => {
-        console.log("Found last dandy node", dandy)
-        dandy.update_js()
-      })
-    }
+    const texty_widget = add_simple_node(node, "pre", texty)
+    texty_widget.serialize = false
 
     this.load_p5js()
   }
 
   async load_p5js() {
-    const response = await fetch(`${extension_webroot}p5/p5.js_`)
+    const response = await fetch(`${dandy_webroot}p5/p5.js_`)
     const text = await response.text()
     this.js_blob = new Blob([text], { type: 'application/javascript' })
     this.js_url = URL.createObjectURL(this.js_blob)
-    console.log("Loaded P5js", this.js_url)
-    this.loaded = true
 
-    this.update_js()
-    find_last_js_in_chain(this, (dandy) => {
-      console.log("Found last dandy node", dandy)
-      dandy.update_js()
+    this.js_chain.js_urls = this.js_url
+    this.js_chain.update_chain()
+
+    const p = new Promise((resolve, reject) => {
+      const reader = new FileReader()
+
+      reader.onload = function(event) {
+        const content = event.target.result
+        const lines = content.split('\n')
+        const topLine = lines[0]
+        resolve(topLine)
+      }
+
+      reader.onerror = (event) => {
+        reject(event.target.error)
+      }
+
+      reader.readAsText(this.js_blob.slice(0, 1024)) // only the first 1KB
     })
+
+    p.then(topLine => {
+        this.texty.innerText = topLine
+    })
+    .catch(error => {
+        console.error("Error reading top line of p5.js:", error)
+    })
+
+
   }
   
-  update_js() {
-    console.log('DandyP5JsLoader.update_js()')
-    const { in_slot_javascript, out_slot_javascript, node, js_url } = this
-    
-    const force_update = true
-    let out_urls = ""
-    if (node.isInputConnected(in_slot_javascript)) {
-      const in_urls = node.getInputData(in_slot_javascript, force_update)
-      console.log("input urls", in_urls, "but is connected", node.isInputConnected(in_slot_javascript))
-      out_urls += `${in_urls}\n`
-    }
+  
 
-    out_urls += js_url
-    console.log('DandyP5JsLoader.update_js() :: out_urls', out_urls)
-    node.setOutputData(out_slot_javascript, out_urls)
-    node.triggerSlot(out_slot_javascript)
-  }
 }
-
 
 // -----------------------------------------------------------------------------------
 const ext = {
@@ -417,7 +583,7 @@ const ext = {
 
     const initDandyNode = (klass) => {
       if (klass)
-        node.dandy = new klass(node)
+        node.dandy = new klass(node, app)
     }
 
     const o = {
