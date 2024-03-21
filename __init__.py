4befa14bb4d8c7911395192f2e5e4fdb7ded17ce
lucas <lucas@example.com>
2024-03-21 00:42:17 -0400

    nodes connecting

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 import torch
 import base64
-import os
 import folder_paths
 from io import BytesIO
 from PIL import Image, ImageOps
@@ -8,89 +7,135 @@ from PIL.PngImagePlugin import PngInfo
 import numpy as np
 
 def image_to_data_url(image):
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    img_base64 = base64.b64encode(buffered.getvalue())
-    return f"data:image/png;base64,{img_base64.decode()}"
+  buffered = BytesIO()
+  image.save(buffered, format="PNG")
+  img_base64 = base64.b64encode(buffered.getvalue())
+  return f"data:image/png;base64,{img_base64.decode()}"
 
 class DandyEditor:
-    def __init__(self):
-        self.updateTick = 1
-        pass
+  def __init__(self):
+    self.updateTick = 1
+    pass
 
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-            }, 
-            "hidden": {
-               "unique_id":"UNIQUE_ID",
-            },
-            "optional": {
-                "images": ("IMAGE",),
-            },
+  @classmethod
+  def INPUT_TYPES(s):
+    return {
+      "required": {
+      }, 
+      "hidden": {
+      },
+      "optional": {
+        "seed": ("SEED", 0),
+      },
+    }
 
-        }
+  RETURN_TYPES = ()
+  RETURN_NAMES = ()
 
-    RETURN_TYPES = ()
+  FUNCTION = "run"
+  OUTPUT_NODE = True
+  CATEGORY = "DandyLand"
 
-    FUNCTION = "run"
+  def IS_CHANGED(self):
+    self.updateTick += 1
+    return hex(self.updateTick)
 
-    OUTPUT_NODE = True
+  def run(self):
+    collected_images = list()
+    if images is not None:
+      for image in images:
+        i = 255. * image.cpu().numpy()
+        img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8)) 
+        collected_images.append(image_to_data_url(img))
 
-    CATEGORY = "DandyLand"
+    return { "ui": {"collected_images":collected_images} }
 
-    def IS_CHANGED(self, unique_id, images):
-        self.updateTick += 1
-        return hex(self.updateTick)
+class DandyJsLoader:
+  def __init__(self):
+    self.updateTick = 1
+    pass
 
-    def run(self, unique_id,  images=None):
+  @classmethod
+  def INPUT_TYPES(s):
+    return {
+      "required": {
+      }, 
+      "hidden": {
+      },
+      "optional": {
+      },
+    }
 
-        collected_images = list()
-        if images is not None:
-            for image in images:
-                i = 255. * image.cpu().numpy()
-                img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8)) 
-                collected_images.append(image_to_data_url(img))
+  RETURN_TYPES = ()
+  RETURN_NAMES = ()
+  FUNCTION = "run"
+  OUTPUT_NODE = True
+  CATEGORY = "DandyLand"
 
+  def IS_CHANGED(self, js=None):
+    self.updateTick += 1
+    return hex(self.updateTick)
 
-        return { "ui": {"collected_images":collected_images}}
+  def run(self, js=None):
+    pass
+
+class DandyP5JsLoader:
+  def __init__(self):
+    self.updateTick = 1
+    pass
+
+  @classmethod
+  def INPUT_TYPES(s):
+    return {
+      "required": {
+      }, 
+      "hidden": {
+      },
+      "optional": {
+      },
+    }
+
+  RETURN_TYPES = ()
+  RETURN_NAMES = ()
+  FUNCTION = "run"
+  OUTPUT_NODE = True
+  CATEGORY = "DandyLand"
+
+  def IS_CHANGED(self):
+    self.updateTick += 1
+    return hex(self.updateTick)
+
+  def run(self):
+    # print("running p5js loader..")
+    # print("js: " + str(js_in))
+    # print("p5js: " + str(p5js))
+    # return(str(js_in) + '\n' + str(p5js))
+    pass
 
 
 class DandyCanvas:
-    """
-    A Image Buffer for handling an editor in another tab.
-    """
+  def __init__(self):
+    pass
+  
+  @classmethod
+  def INPUT_TYPES(s):
+    return {
+      "required": {
+      }, 
+      "hidden": {
+      },
+      "optional": {
+      },
+    }
 
-    def __init__(self):
-        pass
-    
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "mask": ("STRING",""),                                
-                "canvas": ("STRING",""),
-            }, 
-            "hidden": {
-               "unique_id":"UNIQUE_ID",
-            },
-#            "optional": {
-#                "images": ("IMAGE",),
-#            },
+  RETURN_TYPES = ("IMAGE", "MASK",)
+  RETURN_NAMES = ("Image", "Mask",)
 
-        }
+  FUNCTION = "image_buffer"
+  OUTPUT_NODE = True
+  CATEGORY = "DandyLand"
 
-    RETURN_TYPES = ("IMAGE","MASK")
-
-    FUNCTION = "image_buffer"
-
-    #OUTPUT_NODE = False
-
-    CATEGORY = "DandyLand"
-
-    def image_buffer(self, unique_id, mask, canvas, images=None):
-
+  def image_buffer(self, js):
 #        collected_images = list()
 #        if images is not None:
 #            for image in images:
@@ -99,41 +144,39 @@ class DandyCanvas:
 #                collected_images.append(image_to_data_url(img))
 #
 #        print(f"Node {unique_id}: images: {images}")    
+      image_path = folder_paths.get_annotated_filepath("canvas")
+      i = Image.open(image_path)
+      i = ImageOps.exif_transpose(i)
 
-        image_path = folder_paths.get_annotated_filepath(canvas)
-        i = Image.open(image_path)
-        i = ImageOps.exif_transpose(i)
+      rgb_image = i.convert("RGB")
+      rgb_image = np.array(rgb_image).astype(np.float32) / 255.0
+      rgb_image = torch.from_numpy(rgb_image)[None,]
 
-        rgb_image = i.convert("RGB")
-        rgb_image = np.array(rgb_image).astype(np.float32) / 255.0
-        rgb_image = torch.from_numpy(rgb_image)[None,]
+      mask_path = folder_paths.get_annotated_filepath(mask)
+      i = Image.open(mask_path)
+      i = ImageOps.exif_transpose(i)
 
+      if "A" in i.getbands():
+          mask_data = np.array(i.getchannel("A")).astype(np.float32) / 255.0
+          mask_data = torch.from_numpy(mask_data)
+      else:
+          mask_data = torch.zeros((64,64), dtype=torch.float32, device="cpu")
 
-        mask_path = folder_paths.get_annotated_filepath(mask)
-        i = Image.open(mask_path)
-        i = ImageOps.exif_transpose(i)
-
-        if 'A' in i.getbands():
-            mask_data = np.array(i.getchannel('A')).astype(np.float32) / 255.0
-            mask_data = torch.from_numpy(mask_data)
-        else:
-            mask_data = torch.zeros((64,64), dtype=torch.float32, device="cpu")
-
-
-        
-
-        return (rgb_image, mask_data) 
-
+      return (rgb_image, mask_data, js) 
 
 # Set the web directory, any .js file in that directory will be loaded by the frontend as a frontend extension
 WEB_DIRECTORY = "web"
 
 NODE_CLASS_MAPPINGS = {
-    "DandyEditor": DandyEditor,
-    "DandyCanvas": DandyCanvas
+  "DandyEditor": DandyEditor,
+  "DandyCanvas": DandyCanvas,
+  "DandyJsLoader": DandyJsLoader,
+  "DandyP5JsLoader": DandyP5JsLoader
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "DandyEditor": "Dandy Editor",
-    "DandyCanvas": "Dandy Canvas"
+  "DandyEditor": "Dandy Editor",
+  "DandyCanvas": "Dandy Canvas",
+  "DandyJsLoader": "Dandy Js Loader",
+  "DandyP5JsLoader": "Dandy p5.js Loader"
 }
--- web/main.js
+++ web/main.js
@@ -1,11 +1,13 @@
-import { app } from "../../scripts/app.js";
+import { app } from "../../scripts/app.js"
 import { api } from "../../scripts/api.js"
-import { ComfyWidgets } from "../../scripts/widgets.js";
-//import { ace } from "./ace/ace.js";
+import { ComfyWidgets } from "../../scripts/widgets.js"
+//import { ace } from "./ace/ace.js"
 
 const extension_name = "dandy"
 const extension_webroot = "/extensions/dandy/"
 const extension_css = `${extension_webroot}dandy.css`
+const JS_TYPE = 'JS_URLS'
+const JS_NAME = 'js'
 
 const loadDandyCss = (doc) => {
   const dandycss = doc.createElement("link")
@@ -13,6 +15,14 @@ const loadDandyCss = (doc) => {
   dandycss.type = 'text/css'
   dandycss.href = `${extension_css}`
   doc.head.appendChild(dandycss)
+}
+
+const connectJsIo = (dandy) => {
+  const { node } = dandy
+  node.addInput(JS_NAME, JS_TYPE)
+  node.addOutput(JS_NAME, JS_TYPE)
+  dandy.in_slot_javascript = node.findInputSlot(JS_NAME)
+  dandy.out_slot_javascript = node.findOutputSlot(JS_NAME)
 }
 
 const dandyCssLink = `<link rel="stylesheet" type="text/css" href="${extension_css}" />`
@@ -24,10 +34,10 @@ const initDandy = () => {
   loadDandyCss(document)
 
   const pairwise = (a) => {
-    const x = [];
+    const x = []
     for (let i = 0; i < a.length; i += 2)
-      x.push([a[i], a[i + 1]]);
-    return x;
+      x.push([a[i], a[i + 1]])
+    return x
   }
   
   // comfyui will try to load these if we leave them as .js files.
@@ -45,17 +55,17 @@ const initDandy = () => {
   let i_feature = 0
 
   pairwise(features_and_codes).forEach(async pair => {
-    const [feature, js_] = pair;
-    const response = await fetch(`${extension_webroot}ace/${js_}`);
-    const text = await response.text();
-    const blob = new Blob([text], { type: 'application/javascript' });
-    const url = URL.createObjectURL(blob);
-    ace.config.setModuleUrl(`ace/${feature}`, url);
-    console.log(`${i_feature}/${n_features}:ace.config.setModuleUrl("ace/${feature}", ${url});`)
+    const [feature, js_] = pair
+    const response = await fetch(`${extension_webroot}ace/${js_}`)
+    const text = await response.text()
+    const blob = new Blob([text], { type: 'application/javascript' })
+    const url = URL.createObjectURL(blob)
+    ace.config.setModuleUrl(`ace/${feature}`, url)
+    console.log(`${i_feature}/${n_features}:ace.config.setModuleUrl("ace/${feature}", ${url})`)
     if (++i_feature == n_features)
       console.log("DANDY_INITIALIZED")
       DANDY_INITIALIZED = true
-  });
+  })
 }
 
 
@@ -66,132 +76,332 @@ const simple_widget_getter_setter = (el) => {
 	}
 }
 
-const add_simple_node = (node, el) => {
-  console.log("node.addDOMWidget(", el.id, el.id, el, simple_widget_getter_setter(el), ")")
-  return node.addDOMWidget(el.id, el.id, el, simple_widget_getter_setter(el));
+const add_simple_node = (node, type, el) => {
+  console.log("node.addDOMWidget(", el.id, type, el, simple_widget_getter_setter(el), ")")
+  return node.addDOMWidget(el.id, type, el, simple_widget_getter_setter(el))
 }
 
-const initDandyEditorNode = (node) => {
-  console.log("initDandyEditorNode(node)", node)
-  const editor_id = `dandy_editor_${node.id}`
+class DandyEditor {
+  static i_editor = 0
 
-  const dandy_div = document.createElement("div")
-  dandy_div.classList.add("dandy-node")
-  
-  const widget = add_simple_node(node, dandy_div)
-  
-  const editor_pre = document.createElement("pre")
-  editor_pre.classList.add("dandy-editor")
-  editor_pre.id = editor_id
-  editor_pre.style.width = "100%"
-  editor_pre.style.height = "100%"
-  editor_pre.style.marginTop = "0px"
-  editor_pre.style.marginBottom = "0px"
-  
-  dandy_div.appendChild(editor_pre)
-	
-  widget.dandy_div = dandy_div
-  widget.editor_pre = editor_pre
-  
-  const editor = ace.edit(editor_id)
-  editor.setTheme("ace/theme/twilight")
-  editor.session.setMode("ace/mode/javascript")
-  
-  // comfyui uses css transforms 
-  // without this line, cursor postion and mouse clicks won't line up
-  editor.setOption('hasCssTransforms', true)
+  constructor(node) {
+    this.node = node
+    connectJsIo(this)
 
-	editor_pre.addEventListener("resize", (event) => {
-    console.log('Event type:', event.type)
-    console.log('Event details:', event)
-    editor.resize()
-	})
+    console.log("initDandyEditorNode(node)", node)
+    const node_type = 'dandy_editor'
+    const editor_id = `${node_type}_${DandyEditor.i_editor++}`
 
+    const dandy_div = document.createElement("div")
+    dandy_div.classList.add("dandy_node")
+    
+    const widget = add_simple_node(node, node_type, dandy_div)
+    
+    const editor_pre = document.createElement("pre")
+    editor_pre.classList.add("dandy-editor")
+    editor_pre.id = editor_id
+    editor_pre.style.width = "100%"
+    editor_pre.style.height = "100%"
+    editor_pre.style.marginTop = "0px"
+    editor_pre.style.marginBottom = "0px"
+    
+    dandy_div.appendChild(editor_pre)
+    
+    widget.dandy_div = dandy_div
+    widget.editor_pre = editor_pre
+    
+    const editor = ace.edit(editor_id)
+    editor.setTheme("ace/theme/twilight")
+    editor.session.setMode("ace/mode/javascript")
+    
+    // comfyui uses css transforms 
+    // without this line, cursor postion and mouse clicks won't line up
+    editor.setOption('hasCssTransforms', true)
+
+    editor_pre.addEventListener("resize", (event) => {
+      console.log('Event type:', event.type)
+      console.log('Event details:', event)
+      editor.resize()
+    })
+  }
 }
 
-let iDandies = 0
-const getNextId = () => {
-  return iDandies++
-}
+class DandyCanvas {
+  constructor(node) {
+    this.node = node
+    connectJsIo(this)
 
-const initDandyCanvasNode = async (node) => {
-  const dandyland = document.createElement("iframe")
-  dandyland.classList.add('dandyMax')
-  dandyland.id = `dandyland_${getNextId()}`
-
-  const js_files = ["p5/p5.js_"]
-  const n_files = js_files.length
-  const js_blob_urls = []
-
-  for (let i_file = 0; i_file < n_files; i_file++) {
-    const js_ = js_files[i_file]
-    const response = await fetch(`${extension_webroot}${js_}`)
-    const text = await response.text()
-    const blob = new Blob([text], { type: 'application/javascript' })
-    const url = URL.createObjectURL(blob)
-    js_blob_urls.push(url)
+    this.dandyland = null
+    this.widget = null
+    this.js_blob_urls = []
+    //node.addInput("javascript", "JAVASCRIPT")
+    this.load_dandyland()
   }
 
-  const script_tags = js_blob_urls.map((url) => `<script type="application/javascript" src=${url}></script>`)
+  async load_dandyland() {
+    const { js_blob_urls, node } = this
 
-  dandyland.srcdoc = `<html class="dandyMax"><head>${dandyCssLink}</head><body class="dandyMax">
-    ${script_tags.join("\n    ")}
-    <canvas class="dandyMax" />
-  </body></html>`
-  
-  // Add the iframe to the DOM after all scripts are loaded
-  const widget = add_simple_node(node, dandyland)
-  widget.dandyland = dandyland
-  widget.js_blob_urls = js_blob_urls
+    const dandyland = document.createElement("iframe")
+    dandyland.classList.add('dandyMax')
+
+    const js_files = ["p5/p5.js_"]
+    const n_files = js_files.length
+
+    for (let i_file = 0; i_file < n_files; i_file++) {
+      const js_ = js_files[i_file]
+      const response = await fetch(`${extension_webroot}${js_}`)
+      const text = await response.text()
+      const blob = new Blob([text], { type: 'application/javascript' })
+      const url = URL.createObjectURL(blob)
+      js_blob_urls.push(url)
+    }
+
+    const script_tags = js_blob_urls.map((url) => `<script type="application/javascript" src=${url}></script>`)
+
+    dandyland.srcdoc = `<html class="dandyMax"><head>${dandyCssLink}</head><body class="dandyMax">
+      ${script_tags.join("\n    ")}
+      <canvas class="dandyMax" />
+    </body></html>`
+    
+    // Add the iframe to the DOM after all scripts are loaded
+    const widget = add_simple_node(node, "dandyland", dandyland)
+    this.dandyland = dandyland
+    this.widget = widget
+  }
 }
+
+class DandyJsLoader {
+  constructor(node) {
+    this.node = node
+    connectJsIo(this)
+
+    this.files = []
+    this.urlmap = {}
+    this.js_blobs = []
+    this.serialize_widgets = true
+    this.isVirtualNode = true
+      
+    const file_input = document.createElement("input")
+    const js_files_selected = async () => {
+      if (file_input.files.length) {
+        console.log("this.add_js_files(file_input.files)", file_input, file_input.files)
+        await this.add_js_files(file_input.files)
+      }
+    }
+
+    Object.assign(file_input, {
+      type: "file",
+      accept: "application/javascript",
+      style: "display: none",
+      onchange: js_files_selected,
+    })
+    document.body.appendChild(file_input)
+
+    const load_js_widget = node.addWidget("button", "JAVASCRIPT", "javascript", () => {
+      file_input.click()
+    })
+    load_js_widget.label = "Choose files..."
+    load_js_widget.serialize = false
+    console.log('added widget', load_js_widget)
+
+    const files_div = document.createElement("div")
+    files_div.classList.add("dandyMax")
+    
+    const files_widget = add_simple_node(node, "div", files_div)
+    files_widget.serialize = false
+
+    const ul = document.createElement('ul')
+    ul.classList.add('dandyUL')
+    files_div.appendChild(ul)
+
+    this.files.forEach((file, i) => {
+      const li = document.createElement('li')
+      const file_span = document.createElement('span')
+      const move_up_button = document.createElement('span')
+      const move_down_button = document.createElement('span')
+      const remove_button = document.createElement('span')
+      
+      li.classList.add('dandyLI')
+      file_span.classList.add('dandyFileSpan')
+      move_up_button.classList.add('dandySpanButton')
+      move_down_button.classList.add('dandySpanButton')
+      remove_button.classList.add('dandySpanButton')
+
+      file_span.innerText = file
+      move_up_button.innerHTML = "(up)"
+      move_down_button.innerHTML = "(down)"
+      remove_button.innerHTML = "(remove)"
+
+      li.append(file_span, move_up_button, move_down_button, remove_button)
+      ul.append(li)
+    })
+    
+  }
+
+  async add_js_files(chosen_files) {
+    const { files, urlmap, js_blobs } = this
+    for (let i = 0; i < chosen_files.length; ++i) {
+      console.log("files", files)
+      files.push(chosen_files[i])
+    }
+
+    for (let i = 0; i < files.length; ++i) {
+      const file = files[i]
+      console.log("FILE", file)
+      if (urlmap[file] === undefined) {
+        const response = await fetch(`${file}`)
+        const text = await response.text()
+        const blob = new Blob([text], { type: 'application/javascript' })
+        const url = URL.createObjectURL(blob)
+        js_blobs.push(blob)
+        urlmap[file] = url
+      }
+    }
+  }
+}
+
+const find_last_js_in_chain = (dandy, f_taking_dandy, seen = []) => {
+  const { node, out_slot_javascript } = dandy
+  const { graph, outputs } = node
+  const output = outputs[out_slot_javascript]
+  const { links } = output
+  
+  if (links === null || links.length === 0) {
+    f_taking_dandy(dandy)
+    return
+  }
+
+  links.forEach( (link_id) => {
+    const link = graph.links[link_id]
+    const { target_id } = link
+    
+    let loopy = false
+    seen.forEach( (seen_target_id) => {
+      if (seen_target_id === target_id) {
+        loopy = true
+      }
+    })
+    if (loopy) {
+      console.error("loop in graph, bailing")
+      return
+    }
+    const seen_prime = seen.concat([target_id])
+
+    // console.log("TARGET_ID", link.target_id)
+    const target_node = graph.getNodeById(link.target_id)
+    const target_dandy = target_node.dandy
+    find_last_js_in_chain(target_dandy, f_taking_dandy, seen_prime)
+  })
+}
+
+class DandyP5JsLoader {
+  
+  constructor(node) {
+    this.node = node
+    connectJsIo(this)
+
+    node.size = [180, 55]
+
+    this.js_blob = null
+    this.js_url = ""
+    this.serialize_widgets = true
+    this.isVirtualNode = true
+    this.loaded = false
+
+    this.update_js = this.update_js.bind(this)
+
+    console.log("onConnectionsChange", node.onConnectionsChange)
+    node.onConnectionsChange = (type, index, connected, link_info) => {
+      find_last_js_in_chain(this, (dandy) => {
+        console.log("Found last dandy node", dandy)
+        dandy.update_js()
+      })
+    }
+
+    this.load_p5js()
+  }
+
+  async load_p5js() {
+    const response = await fetch(`${extension_webroot}p5/p5.js_`)
+    const text = await response.text()
+    this.js_blob = new Blob([text], { type: 'application/javascript' })
+    this.js_url = URL.createObjectURL(this.js_blob)
+    console.log("Loaded P5js", this.js_url)
+    this.loaded = true
+
+    this.update_js()
+    find_last_js_in_chain(this, (dandy) => {
+      console.log("Found last dandy node", dandy)
+      dandy.update_js()
+    })
+  }
+  
+  update_js() {
+    console.log('DandyP5JsLoader.update_js()')
+    const { in_slot_javascript, out_slot_javascript, node, js_url } = this
+    
+    const force_update = true
+    let out_urls = ""
+    if (node.isInputConnected(in_slot_javascript)) {
+      const in_urls = node.getInputData(in_slot_javascript, force_update)
+      console.log("input urls", in_urls, "but is connected", node.isInputConnected(in_slot_javascript))
+      out_urls += `${in_urls}\n`
+    }
+
+    out_urls += js_url
+    console.log('DandyP5JsLoader.update_js() :: out_urls', out_urls)
+    node.setOutputData(out_slot_javascript, out_urls)
+    node.triggerSlot(out_slot_javascript)
+  }
+}
+
 
 // -----------------------------------------------------------------------------------
 const ext = {
 	// Unique name for the extension
 	name: extension_name,
-	async init(app) {
+	init: async (app) => {
 		// Any initial setup to run as soon as the page loads
 		initDandy()
 	},
-	async setup(app) {
+	setup: async (app) => {
 		// Any setup to run after the app is created
 	},
-	async addCustomNodeDefs(defs, app) {
+	addCustomNodeDefs: async (defs, app) => {
 		// Add custom node definitions
 		// These definitions will be configured and registered automatically
 		// defs is a lookup core nodes, add yours into this
 	},
-	async getCustomWidgets(app) {
+	getCustomWidgets: async (app) => {
 		// Return custom widget types
 		// See ComfyWidgets for widget examples
     return {
-      JAVASCRIPT(node, inputName, inputData, app) {
-        console.log("GET CUSTOME WIGET CANVAS", CANVAS)
-        return addCanvasWidget(node,inputName,inputData, app)
-      } 
+      // JAVASCRIPT(node, inputName, inputData, app) {
+      //   console.log("get custom Javascript widget")
+      //   return add_choose_js_files_widget(node, inputName, inputData, app)
+      // } 
     }
 	},
-	async beforeRegisterNodeDef(nodeType, nodeData, app) {
+	beforeRegisterNodeDef: async (nodeType, nodeData, app) => {
 		// Run custom logic before a node definition is registered with the graph
 
 		// This fires for every node definition so maybe only once
-		//delete ext.beforeRegisterNodeDef;
+		//delete ext.beforeRegisterNodeDef
 	},
-	async registerCustomNodes(app) {
+	registerCustomNodes: async (app) => {
 		// Register any custom node implementations here allowing for more flexability than a custom node def
-    class DandyLand {
-
-    }
+    // LiteGraph.registerNodeType("Dandy Js Loader", DandyJavascriptLoader)
+    // this next category set has to be after registerNodeType, i can't explain why.
+    // DandyJavascriptLoader.category = "DandyLand"
   },
-	loadedGraphNode(node, app) {
+	loadedGraphNode: (node, app) => {
 		// Fires for each node when loading/dragging/etc a workflow json or png
 		// If you break something in the backend and want to patch workflows in the frontend
 		// This is the place to do this
-
 		// This fires for every node on each load so maybe only once
-		//delete ext.loadedGraphNode;
+		//delete ext.loadedGraphNode
 	},
-	nodeCreated(node, app) {
+	nodeCreated: (node, app) => {
     // Fires every time a node is constructed
     // You can modify widgets/add handlers/etc here
 
@@ -205,24 +415,23 @@ const ext = {
       f()
     }
 
-    when_up(() => {
-      const title = node.getTitle();
-      // node.type not set at this point? 
-      switch (title) {
-        case "Dandy Editor":
-          initDandyEditorNode(node);
-          break;
-        case "Dandy Canvas":
-          initDandyCanvasNode(node);
-          break;
-        case "Dandy JavaScript Loader":
-          initDandyJavaScriptLoaderNode(node);
-          break;
-      }
+    const initDandyNode = (klass) => {
+      if (klass)
+        node.dandy = new klass(node)
+    }
 
+    const o = {
+      "Dandy Editor": DandyEditor,
+      "Dandy Canvas": DandyCanvas,
+      "Dandy Js Loader": DandyJsLoader,
+      "Dandy p5.js Loader": DandyP5JsLoader,
+    }
+
+    when_up(() => {
+      initDandyNode(o[node.getTitle()])
     })
 	}
-};
+}
 
-app.registerExtension(ext);
+app.registerExtension(ext)
 
